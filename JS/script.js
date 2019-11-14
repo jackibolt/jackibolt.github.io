4b489c83e28be0070d5d3fa16f8eb58e8d673466
Jacki Bolt <bolt.jacqueline@gmail.com>
2019-11-14 15:37:01 -0500

    more edits

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,19 @@ console.log("It's working");
 
 window.onscroll = function() {scrollFunction()};
 
+const headerName = document.getElementById('header-name');
+const headerLogo = document.getElementById("header-logo");
 
 function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      document.getElementById('header-name').style.display = "none";
-      document.getElementById("header-logo").style.display = "block";
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+      // headerName.style.visibility = "hidden";
+      headerName.style.display = "none";
+      headerLogo.style.display = 'block';
+      // headerLogo.style.visibility = "visible";
     } else {
-      document.getElementById('header-name').style.display = "block";
-      document.getElementById('header-logo').style.display = "none";
+      // headerName.style.visibility = "visible";
+      headerName.style.display = "block";
+      // headerLogo.style.visibility = "hidden";
+      headerLogo.style.display = 'none';
     }
 }
