b676a3cddf48c83758222b6be0839f49a148fa57
Jacqueline Bolt <bolt.jacqueline@gmail.com>
2019-11-14 10:01:28 -0500

    working on nav features

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 
 
 console.log("It's working");
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      document.getElementById('header-name').style.display = "none";
+      document.getElementById("header-logo").style.display = "block";
+    } else {
+      document.getElementById('header-name').style.display = "block";
+      document.getElementById('header-logo').style.display = "none";
+    }
+  }
