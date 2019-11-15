566ca6994555b699e2145da4c821ef321c364ec7
Jacqueline Bolt <bolt.jacqueline@gmail.com>
2019-11-14 20:06:25 -0500

    edits to nav

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,29 @@
 
 console.log("It's working");
 
-window.onscroll = function() {scrollFunction()};
-
 const headerName = document.getElementById('header-name');
 const headerLogo = document.getElementById("header-logo");
+const navDark = document.querySelector('.navbar-dark');
+const navBar = document.querySelector('.navbar'); 
+
+const windowWidth = window.matchMedia("(min-width: 768px)");
+console.log(windowWidth);
+
+if (windowWidth.matches) {
+  window.onscroll = function() {scrollFunction()};
+}
 
 function scrollFunction() {
+    // scroll has passed 450px -> hide wordmark, show circle logo
     if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-      // headerName.style.visibility = "hidden";
-      headerName.style.display = "none";
-      headerLogo.style.display = 'block';
-      // headerLogo.style.visibility = "visible";
+        headerName.style.display = "none";
+        headerLogo.style.display = 'block';
+        navBar.style.height = '110px'
+        navDark.classList.add('navbar-expand-md');
     } else {
-      // headerName.style.visibility = "visible";
+      // scroll has NOT passed 450px -> show wordmark, hide circle logo
       headerName.style.display = "block";
-      // headerLogo.style.visibility = "hidden";
       headerLogo.style.display = 'none';
+      navDark.classList.remove('navbar-expand-md');
     }
 }
